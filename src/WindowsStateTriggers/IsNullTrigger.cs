f275eeaa85cc7c166d1eab258b48b6ffe2c409e0
Scott Lovegrove <scottisafool@live.co.uk>
2015-03-28 17:21:22 +0000

    Added NullTriggers

---
+++ src/WindowsStateTriggers/IsNullTrigger.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml;
+
+namespace WindowsStateTriggers
+{
+    public class IsNullTrigger : StateTriggerBase
+    {
+        public static readonly DependencyProperty ItemProperty = DependencyProperty.Register(
+            "Item", typeof (object), typeof (IsNullTrigger), new PropertyMetadata(default(object), OnItemChanged));
+
+        public object Item
+        {
+            get { return GetValue(ItemProperty); }
+            set { SetValue(ItemProperty, value); }
+        }
+
+        private static void OnItemChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var trigger = sender as IsNullTrigger;
+            var item = e.NewValue;
+
+            trigger?.SetTriggerValue(item == null);
+        }
+    }
+}
