c1f20f82de3ffc6411d1db4b7bc6ba02decd6fd9
Marco Minerva <marco.minerva@gmail.com>
2015-07-03 22:04:14 +0200

    Added EnumStateTrigger.

---
+++ src/TestApp/Samples/EnumSample.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Linq;
+using System.ComponentModel;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TestApp.Samples
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class EnumSample : UserControl, INotifyPropertyChanged
+    {
+        public EnumSample()
+        {
+            this.InitializeComponent();
+        }
+
+        private AccessLevel selectedAccessLevel;
+        public AccessLevel SelectedAccessLevel
+        {
+            get { return selectedAccessLevel; }
+            set {
+                if (selectedAccessLevel != value)
+                {
+                    selectedAccessLevel = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedAccessLevel)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void accessLevelNone_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedAccessLevel = AccessLevel.None;
+        }
+
+        private void accessLevelRead_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedAccessLevel = AccessLevel.Reader;
+        }
+
+        private void accessLevelWrite_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedAccessLevel = AccessLevel.Writer;
+        }
+    }
+
+    public enum AccessLevel
+    {
+        None,
+        Reader,
+        Writer
+    }
+}
