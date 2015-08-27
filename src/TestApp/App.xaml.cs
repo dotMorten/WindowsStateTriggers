02b0dcf3bb2da691c011af6d272e8deb305c4c4a
Scott Lovegrove <scottisafool@live.co.uk>
2015-08-27 05:54:24 +0100

    Changes made as per PR comments

--- src/TestApp/App.xaml.cs
+++ src/TestApp/App.xaml.cs
@@ -25,12 +25,12 @@ namespace TestApp
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
     sealed partial class App : Application
-    {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
+	{
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public App()
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
