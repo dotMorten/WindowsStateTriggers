af0f61cb90256876fc0f184d2bd3b877b66c23eb
Gian Paolo Santopaolo <g_santopaolo@hotmail.com>
2016-01-14 20:25:14 +0100

    Addes InteractionCapabilityTrigger and Salple. With this capability dev can deciede with UI to show depending on the way user(s) are interacting with device Device Capability possibles options: 1. Tablet and Phone  2. Mouse and Keyboard (Classic Desktop) 3. Multiuser MultiTouch (SurfaceHub) That way it is possible to create the right UI

---
+++ src/TestApp/Samples/InteractionCapabilitySample.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace TestApp.Samples
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class InteractionCapabilitySample : Page
+    {
+        public InteractionCapabilitySample()
+        {
+            this.InitializeComponent();
+        }
+
+
+    }
+}
---
+++ src/WindowsStateTriggers/InteractionCapabilityTrigger.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace WindowsStateTriggers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class InteractionCapabilityTrigger : StateTriggerBase, ITriggerValue
+    {
+        private bool m_IsActive;
+
+        /// <summary>
+		/// Occurs when the <see cref="IsActive" /> property has changed.
+		/// </summary>
+        public event EventHandler IsActiveChanged;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InteractionCapabilityTrigger"/> class.
+        /// </summary>
+        public InteractionCapabilityTrigger()
+        {
+            if (!Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                var weakEvent =
+                    new WeakEventListener<InteractionCapabilityTrigger, object, WindowSizeChangedEventArgs>(this)
+                    {
+                        OnEventAction = (instance, source, eventArgs) => InteractionCapabilitiesTrigger_SizeChanged(source, eventArgs),
+                        OnDetachAction = (instance, weakEventListener) => Window.Current.SizeChanged -= weakEventListener.OnEvent
+                    };
+                Window.Current.SizeChanged += weakEvent.OnEvent;
+                UpdateTrigger(InteractionMode);
+            }
+        }
+
+        /// <summary>
+		/// Gets a value indicating whether this trigger is active.
+		/// </summary>
+		/// <value><c>true</c> if this trigger is active; otherwise, <c>false</c>.</value>
+        public bool IsActive
+        {
+            get { return m_IsActive; }
+            private set
+            {
+                if (m_IsActive != value)
+                {
+                    m_IsActive = value;
+                    base.SetActive(value);
+                    if (IsActiveChanged != null)
+                        IsActiveChanged(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        /// <summary>
+		/// Gets or sets the InteractionMode to trigger on.
+		/// </summary>
+        public InteractionCapability InteractionMode
+        {
+            get { return (InteractionCapability)GetValue(InteractionModeProperty); }
+            set { SetValue(InteractionModeProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="InteractionMode"/> parameter.
+        /// </summary>
+        public static readonly DependencyProperty InteractionModeProperty =
+            DependencyProperty.Register("InteractionMode", typeof(InteractionCapability), typeof(InteractionCapabilityTrigger),
+            new PropertyMetadata(InteractionCapability.SingleUserMultitouch, OnInteractionModeChanged));
+
+        private static void OnInteractionModeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = (InteractionCapabilityTrigger)d;
+            if (!Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                var result = (InteractionCapability)e.NewValue;
+                obj.UpdateTrigger(result);
+            }
+        }
+
+        private void UpdateTrigger(InteractionCapability interactionMode)
+        {
+            IsActive = interactionMode == InteractionCapabilityHelper.GetForCurrentView();
+        }
+
+        private void InteractionCapabilitiesTrigger_SizeChanged(object sender, WindowSizeChangedEventArgs e)
+        {
+            UpdateTrigger(InteractionMode);
+        }
+
+
+    }
+}
